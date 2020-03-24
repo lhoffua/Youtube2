2c41ff00887228937f9509980d5a6abbc360adaf
gdadulla <gdadulla@albany.edu>
2020-03-23 22:43:00 -0400

    Register now populates database with user input. Hashes password as well

--- src/server/server.js
+++ src/server/server.js
@@ -1,23 +1,45 @@
-const mongo = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://gdadulla:34rtsdcv@cluster0-mno4t.mongodb.net/test?retryWrites=true&w=majority";
-const client = new mongo(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true});
+
 const socketClient = require('socket.io').listen(4000).sockets;
 
 const bcrypt = require('bcrypt');
 const SALT_WORK_FACTOR = 10;
+client.connect(err => {
+    try{
+    const collection = client.db("login").collection("users");
+    console.log("Connected to database");
+ 
 
-try{
-    client.connect();
-    console.log("MongoDB successfully connected");
+        socketClient.on('connection', function(socket){
+            socket.on('submit', function(data){
+                    let user = data.name;
+                    let pwd = data.password;
 
-    socketClient.on('connection', function(socket){
-        let login = db.collection('login');
-        socket.on('submit', function(data){
 
-        })
-    })
-}
+                    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+                        if(err) throw err;
+                        bcrypt.hash(pwd, salt, function(err, hash){
+                            if(err) throw err;
 
-catch(e){
-    console.error(e);
-}
+                            pwd = hash;
+
+                            collection.insert({username: user, password: pwd});
+                        });
+                    });
+            });
+        });
+    }
+
+    catch{
+        console.log(err);
+    }
+    
+  
+   
+
+
+       
+     
+});
